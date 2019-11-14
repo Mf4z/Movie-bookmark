ea3f950681974b2682df2e91cb34bcd465a3957b
Marwan <emef4z@gmail.com>
2019-11-14 20:29:02 +0400

    Add initializeDisplayContent method

--- app/src/main/java/com/emef4z/moviebookmark/WatchingFragment.java
+++ app/src/main/java/com/emef4z/moviebookmark/WatchingFragment.java
@@ -8,22 +8,39 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class WatchingFragment extends Fragment {
 
-    public WatchingFragment() {
+    private MovieBookmarkOpenHelper mOpenHelper;
+    private RecyclerView mRecyclerItems;
+    private LinearLayoutManager mLinearLayoutManager;
 
-    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+        mOpenHelper = new MovieBookmarkOpenHelper(getContext());
 
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
+        View rootView = inflater.inflate(R.layout.display_list, container, false);
 
 
         return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        mOpenHelper.close();
+        super.onDestroyView();
+    }
+
+
+    private void initializeDisplayContent(View rootView){
+
+        mRecyclerItems = (RecyclerView)rootView.findViewById(R.id.recyclerView_list);
+        mLinearLayoutManager = new LinearLayoutManager(getContext());
+
     }
 }
